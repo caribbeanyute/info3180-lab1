030be829273ddd34a976512577f472feabdf6f92
caribbeanyute <smithcleon@gmail.com>
2020-01-28 14:17:50 +0000

    Added About Page

--- app.py
+++ app.py
@@ -16,12 +16,15 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return 'My Home Page'
-
+    
+@app.route('/about')
+def about():
+    return  render_template('about.html')
+ 
 @app.errorhandler(404)
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
-
 
 if __name__ == '__main__':
     app.run(debug=True,host="0.0.0.0",port=8080)
